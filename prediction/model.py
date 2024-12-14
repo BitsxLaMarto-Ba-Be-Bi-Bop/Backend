eae8d0ee52ac214801f8822304a57be3c7b9f235
Ton Llucià <t.llucia@ubiquat.com>
2024-12-14 22:24:31 +0100

    fix

---
+++ prediction/model.py
@@ -0,0 +1,11 @@
+# from sqlalchemy import Column, ForeignKey, Integer
+# from base.base_model import BaseModel
+# class Prediction(BaseModel):
+#     __tablename__ = 'prediction'
+#     # Columns
+#     id = Column(Integer, primary_key=True)
+#     patient_id=Column(Integer, ForeignKey('user_patient.id'))
+#     date: date
+#     transplantable: bool = Column(Boolean)
+    
+    
